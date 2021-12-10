3a6e1761a036e79f861b177a91a009b7b3962fff
Adam Bilchouris <thesteamyocelot@gmail.com>
2021-12-10 15:08:12 +1100

    dataNew, dataSelection are from realestate.com.au. Cannot get data now, rejects bot requests. selection is a few suburbs of melbourne (38) from different areas. verifyCsv.py just throws an error if the csv is malformed.

---
+++ verifyCsv.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import pandas as pd
+
+#https://www.pythoncentral.io/how-to-traverse-a-directory-tree-in-python-guide-to-os-walk/
+i = 0
+totalProperties = 0
+hasNoneDir = 'selection'
+for dirName, subdirList, fileList in os.walk(f'{hasNoneDir}/'):
+    for fname in fileList:
+        filePath = f'{dirName}/{fname}'
+        csv = pd.read_csv(filePath)
+        print(f'{filePath} : {csv.shape}')
+        i = i + 1
+        totalProperties = totalProperties + csv.shape[0]
+
+print(f'Total suburbs = {i//5}')
+print(f'Total properties = {totalProperties}')
