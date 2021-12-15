bca995de8f83cd6259c332817fe46a88a76030bc
Adam Bilchouris <thesteamyocelot@gmail.com>
2021-12-15 11:45:52 +1100

    Removes missing data for bedrooms, bathrooms, parking spaces, and land size.

---
+++ removeMissing.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+
+READ_DIR = 'combinedFeatures'
+WRITE_DIR = 'combinedFeaturesRemoved'
+
+header = ('"address","price","date","bedrooms","bathrooms","parking_spaces","land_size","land_size_unit","propertyType","url",'
+          '"houseFeatures","schoolsDistance","schoolsCount","salesHistory","heating","air_con","internal_laundry","built_in_wardrobes",'
+          '"secure_parking","study","close_to_shops","close_to_transport","close_to_schools","split_system","fully_fenced","solar_panels",'
+          '"dishwasher","garage","alarm","garden","deck","balcony","pool","spa"')
+
+for dirName, subdirList, fileList in os.walk(f'{READ_DIR}/'):
+    for fname in fileList:
+        filePath = f'{dirName}{fname}'
+        #if 'box-hill' in filePath:
+        #    continue
+        csv = pd.read_csv(filePath)
+        print(filePath)
+        # Remove properties if they have missing values
+        csv = csv[csv['price'].astype(str).astype(int) >= 0]
+        # Remove properties if they have missing values
+        csv = csv[csv['bathrooms'] != -1]
+        csv = csv[csv['parking_spaces'] != -1]
+        csv = csv[csv['land_size'] != -1]
+        #csv = csv.dropna()
+        csv['houseFeatures'] = csv['houseFeatures'].astype(str).str.replace('"', '')
+        csv['houseFeatures'] = csv['houseFeatures'].str.replace('nan', '')
+
+        csv.to_csv(f'{WRITE_DIR}/{fname}', index=False, header=header)
---
+++ verifyCombined.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+
+#https://www.pythoncentral.io/how-to-traverse-a-directory-tree-in-python-guide-to-os-walk/
+i = 0
+totalProperties = 0
+hasNoneDir = 'combinedFeaturesRemoved'
+for dirName, subdirList, fileList in os.walk(f'{hasNoneDir}/'):
+    for fname in fileList:
+        filePath = f'{dirName}{fname}'
+        csv = pd.read_csv(filePath)
+        print(f'{filePath} : {csv.shape}')
+        i = i + 1
+        totalProperties = totalProperties + csv.shape[0]
+
+print(f'Total suburbs = {i}')
+print(f'Total properties = {totalProperties}')
